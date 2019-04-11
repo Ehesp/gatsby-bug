1f04b4824e3b8964c8c4d7b9251cd696e8e4b446
ehesp <elliot.hesp@gmail.com>
2019-04-11 12:00:53 +0100

    init

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,83 @@
  */
 
 // You can delete this file if you're not using it
+exports.sourceNodes = ({ actions, getNode, createNodeId, createContentDigest }) => {
+  const { createNode, createPage, createParentChildLink, createRedirect } = actions;
+
+  // Parent
+  const parentId = createNodeId(`test-parent`);
+
+  createNode({
+    id: parentId,
+    name: 'Parent',
+    type: 'parent',
+    internal: {
+      type: 'DocumentationPage',
+      contentDigest: createContentDigest({ name: 'Parent' }),
+    },
+  });
+
+  // Children
+  const child1Id = createNodeId(`test-child-1`);
+  const child2Id = createNodeId(`test-child-2`);
+
+  createNode({
+    id: child1Id,
+    name: 'Child 1',
+    type: 'child',
+    internal: {
+      type: 'DocumentationPage',
+      contentDigest: createContentDigest({ name: 'Child 1' }),
+    },
+  });
+
+  createNode({
+    id: child2Id,
+    name: 'Child 2',
+    type: 'child',
+    internal: {
+      type: 'DocumentationPage',
+      contentDigest: createContentDigest({ name: 'Child 2' }),
+    },
+  });
+
+  // // Linking - works here
+  // createParentChildLink({
+  //   parent: getNode(parentId),
+  //   child: getNode(child1Id),
+  // });
+  //
+  // createParentChildLink({
+  //   parent: getNode(parentId),
+  //   child: getNode(child2Id),
+  // });
+}
+
+exports.createPages = async ({ graphql, actions, getNode }) => {
+  const { createParentChildLink } = actions;
+
+  const query = await graphql(`
+    {
+      allDocumentationPage {
+        edges {
+          node {
+            id
+            name
+            type
+          }
+        }
+      }
+    }
+  `);
+
+  // Fails:
+  query.data.allDocumentationPage.edges.forEach((node) => {
+    if (node.type === 'child') {
+      console.log('Creating link...')
+      createParentChildLink({
+        parent: getNode(`test-parent`),
+        child: node,
+      });
+    }
+  });
+};
